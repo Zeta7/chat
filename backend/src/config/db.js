387b1fb9bf5a9c7153009ebc054db153328ad846
Edson <edson.joan.m.o97@gmail.com>
2024-08-16 16:51:59 -0500

    fix: correccion de conexxiona la bd

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'message',
-  password: 'root',
+  user: 'message_lpr0_user',
+  host: 'dpg-cqvsbmtumphs73fu73ig-a.oregon-postgres.render.com',
+  database: 'message_lpr0',
+  password: 'VG2bk2LyIZqSbPRfcG2TTpt9xwE77lch',
   port: 5432,
 });
 
