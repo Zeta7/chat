8e0dd8c699707bc1d52404f37239d6b93294a7ec
Edson <edson.joan.m.o97@gmail.com>
2024-08-16 16:08:58 -0500

    proyecto chat

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'message',
+  password: 'root',
+  port: 5432,
+});
+
+const query = (text, params) => pool.query(text, params);
+
+module.exports = {
+  query,
+};
